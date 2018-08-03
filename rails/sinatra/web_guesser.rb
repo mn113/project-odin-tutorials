be643636e78423ecde7974903a2145ec160cd95d
Martin Nicholson <recyclebing+github@gmail.com>
2018-08-03 20:12:42 +0100

    Working guess response

--- rails/sinatra/web_guesser.rb
+++ rails/sinatra/web_guesser.rb
@@ -1,26 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 prng = Random.new
 number = 0
 
 get '/newnumber' do
     number = prng.rand(100) + 1     # generates 0-99
-    erb :index, :locals => {:number => number}
+    erb :index, :locals => {
+        data: {
+            :number => number,
+            :guess => nil
+        }
+    }
 end
 
-post '/guess/:guess' do
-    if params['guess'] > number + 20
-        return 'Ho Lee Fuk'
-    elsif params['guess'] > number
-        return 'Hi'
-    elsif params['guess'] < number
-        return 'Lo'
-    elsif params['guess'] < number - 20
-        return 'Wi Tu Lo'
-    elsif params['guess'] == number
-        return 'Correct'
+get '/guess/' do
+    guess = params['guess'].to_i
+    erb :index, :locals => {
+        data: {
+            :number => number,
+            :guess => guess,
+            :analysis => analyse_guess(guess, number)
+        }
+    }
+end
+
+def analyse_guess(guess, number)
+    if guess > number + 20
+        'Way too high'
+    elsif guess > number
+        'Too high'
+    elsif guess < number
+        'Too low'
+    elsif guess < number - 20
+        'Way too low'
+    elsif guess == number
+        'CORRECT!'
     end
 end
-
