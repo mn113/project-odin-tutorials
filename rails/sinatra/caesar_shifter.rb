a18b4933413b52269be44e71a93232704db96a9c
Martin Nicholson <recyclebing+github@gmail.com>
2018-08-04 13:16:23 +0100

    Finished Caesar shifter template

--- rails/sinatra/caesar_shifter.rb
+++ rails/sinatra/caesar_shifter.rb
@@ -1,30 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/:s?' do |s|
-    offset = s ? s.to_i : 0
-    # erb :index, :locals => {
-    #     data: {
-    #         :text => nil,
-    #         :offset => nil
-    #     }
-    # }
-    #caesar("the quick brown fox jumps over a lazy dog.", offset)
-    #caesar("abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3)
-    caesar("the quick brown fox jumps over a lazy dog.".upcase, offset)
+get '/' do
+    erb :caesar_shifter
+end
+
+post '/' do
+    @offset = params['offset'].to_i
+    @text = caesar(params['text'], @offset)
+    erb :caesar_shifter
 end
 
 def caesar(text, offset)
     text.chars.map{ |c|
         if (65..91).include?(c.ord) # UPPERCASE
             d = c.ord + offset
-            d -= 26 if d >= 91
-            d += 26 if d < 65
+            d -= 26 while d >= 91
+            d += 26 while d < 65
             d.chr
         elsif (97..123).include?(c.ord) # lowercase
             d = c.ord + offset
-            d -= 26 if d >= 123
-            d += 26 if d < 97
+            d -= 26 while d >= 123
+            d += 26 while d < 97
             d.chr
         else # non-letter
             c
