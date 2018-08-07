986d941d4bfa3f8b4f302e67517a8ecc95bdad6b
Martin Nicholson <recyclebing+github@gmail.com>
2018-08-07 18:00:07 +0100

    Tagging completed with index & destroy actions & views

--- rails/blogger/app/controllers/tags_controller.rb
+++ rails/blogger/app/controllers/tags_controller.rb
@@ -2,4 +2,21 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
     end
+
+    def index
+        @tags = Tag.all
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        puts @tag
+        # first, delete all the Taggings records which reference this Tag:
+        Tagging.where(tag_id: @tag.id).each{ |t| t.destroy }
+
+        # now we're safe to proceed:
+        flash.notice = "Tag '#{@tag}' deleted!"
+        @tag.destroy
+
+        redirect_to action: "index"
+    end
 end
